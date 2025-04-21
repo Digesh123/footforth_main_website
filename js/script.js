28dbf6f88fe217f7b3ee3a59032a750ba7c05df0
Digesh123 <digeshghevade123@gmail.com>
2025-04-21 20:38:14 +0530

    p4

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-// Toggle mobile navbar
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-    const navLinks = document.querySelector(".navbar ul");
-  
-    hamburger.addEventListener("click", () => {
-      navLinks.classList.toggle("show");
-    });
-  });
-  
+const navbar = document.querySelector('.navbar');
+const menuToggle = document.getElementById('mobile-menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    navbar.classList.add('scrolled');
+    navbar.classList.remove('transparent');
+  } else {
+    navbar.classList.remove('scrolled');
+    navbar.classList.add('transparent');
+  }
+});
+
+menuToggle.addEventListener('click', () => {
+  mobileMenu.classList.toggle('show');
+});
