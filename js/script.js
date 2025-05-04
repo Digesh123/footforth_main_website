f0b496468f580be8f21b2a627c1d41f6bd17036f
Digesh123 <digeshghevade123@gmail.com>
2025-05-04 18:19:30 +0530

    p9z

--- js/script.js
+++ js/script.js
@@ -20,13 +20,3 @@ AOS.init({
   duration: 1000,
   once: true
 });
-
-
-const acc = document.querySelectorAll(".accordion");
-  acc.forEach(btn => {
-    btn.addEventListener("click", function() {
-      this.classList.toggle("active");
-      let panel = this.nextElementSibling;
-      panel.style.display = panel.style.display === "block" ? "none" : "block";
-    });
-  });
